7c34de03f8666a2e2e018c90673a40cced4e5152
Tatiana <Dok_T@mail.ru>
2024-02-04 21:41:43 +0500

    Создала программу

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] initialArray = new string[] {"Hello", "2", "World", ":-)","Ура"};
+string[] createdArray = new string [initialArray.Length];
+int createdIndex = 0;
+
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <=3)
+    {
+        createdArray[createdIndex] = initialArray[i];
+        createdIndex++;
+    }
+}
+
+for (int i = 0; i < createdIndex; i++)
+{
+    Console.WriteLine(createdArray[i]);
+}
